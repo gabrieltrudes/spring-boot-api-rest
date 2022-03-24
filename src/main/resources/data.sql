df78c85195d9847c5bc16dcca6667f2813fff0f0
Gabriel Trudes Melo <esquecimeunome00@hotmail.com>
2022-03-24 11:09:30 -0300

    Criptografando senha e implementando interfaces para configuração de autenticação do usuário

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$waod5XWK9uPZpaX/fnT.vutHen9YgGWFUIn9PezlsVkFRcP.OO5ty');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
